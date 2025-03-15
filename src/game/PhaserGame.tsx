3cee81443eec5dfdbef1f66d71be11f6f2fe98d5
sutigit <panyanilben@gmail.com>
2025-03-15 19:50:59 +0200

    refactor

--- src/game/PhaserGame.tsx
+++ src/game/PhaserGame.tsx
@@ -1,7 +1,35 @@
 import { forwardRef, useEffect, useLayoutEffect, useRef } from "react";
-import StartGame from "./main";
 import { EventBus } from "./EventBus";
+import { AUTO, Game } from "phaser";
+import { GameScene } from "./scenes/GameScene";
 
+const WINDOW_WIDTH = window.innerWidth;
+const WINDOW_HEIGHT = window.innerHeight;
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: AUTO,
+    width: WINDOW_WIDTH,
+    height: WINDOW_HEIGHT,
+    parent: "game-container",
+    backgroundColor: "#242424",
+    physics: {
+        default: "matter",
+        matter: {
+            debug: false,
+            gravity: {
+                y: 0,
+                x: 0,
+            },
+            enableSleeping: true,
+        },
+    },
+    // I guess this is sort of the activation order of the scenes
+    scene: [GameScene],
+};
+
+const StartGame = (parent: string) => {
+    return new Game({ ...config, parent });
+};
 export interface IRefPhaserGame {
     game: Phaser.Game | null;
     scene: Phaser.Scene | null;
